a5717cf8f8efea606db9db212f41c401eda52f22
firefighter198 <brausjonas@gmail.com>
2021-10-10 04:58:52 +0200

    Basic game logic

--- Jumpyy/Assets/Scripts/Menu/PanelSelectWorld.cs
+++ Jumpyy/Assets/Scripts/Menu/PanelSelectWorld.cs
@@ -1,15 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PanelSelectWorld : MonoBehaviour
 {
     [SerializeField] private GameObject[] arraysNext;
     [SerializeField] private TransitionAnimation transition;
+    private int index;
 
-    public void ButtonWorldSelect()
+    public void ButtonWorldSelect(Button button)
     {
-        
+        index = int.Parse(button.name.Substring(12, 1)) - 1;
+
+        transition.Play();
+    }
+
+    private void Update()
+    {
+        if (transition.isEndingFinished)
+        {
+            switch (index)
+            {
+                case 0:
+                    SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+                    break;
+            }
+        }
     }
 }
